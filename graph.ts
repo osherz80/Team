994e9185f2280f74ed0f80c67dbd4f5d34af88f2
Osher <osherzohar80@gmail.com>
2026-08-21 02:32:00 +0300

    first graph

---
+++ graph.ts
@@ -0,0 +1,24 @@
+import { StateGraph, START, END } from "@langchain/langgraph";
+import { AgentState } from "./state.js";
+import { teamLeadNode } from "./nodes.js";
+
+const builder = new StateGraph(AgentState)
+    .addNode("teamLead", teamLeadNode)
+    .addEdge(START, "teamLead")
+    .addEdge("teamLead", END);
+
+export const graph = builder.compile();
+
+async function main() {
+    const initialInput = {
+        task: "our mission is to build a carbon copy of netflix, it must be also operational like netflix and support the same users count",
+    };
+
+    const result = await graph.invoke(initialInput);
+    console.log(result.subtasks)
+    result.subtasks.forEach((subtask: string, index: number) => {
+        console.log(`${index + 1}. ${subtask}`);
+    });
+}
+
+main().catch(console.error);
